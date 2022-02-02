5f18e69c615ac280914b45b69aa89cc6fa3a2b3f
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2022-02-02 11:39:55 +0200

    abstracting database access

--- spec/e2e/apiSpec.js
+++ spec/e2e/apiSpec.js
@@ -3,6 +3,14 @@ var request = require("request");
 
 describe("The API", function () {
   it("should respond to a GET request at /api/keywords/", function (done) {
+    var expected = {
+      _items: [
+        { id: 1, value: "Aubergine", catergoryID: 1 },
+        { id: 2, value: "Onion", catergoryID: 1 },
+        { id: 3, value: "Knife", catergoryID: 2 },
+      ],
+    };
+
     request.get(
       {
         url: "http://localhost:8080/api/keywords/",
@@ -10,7 +18,7 @@ describe("The API", function () {
       },
       function (err, res, body) {
         expect(res.statusCode).toBe(200);
-        expect(body.foo).toEqual("bar");
+        expect(body.foo).toEqual(expected);
         done();
       }
     );
